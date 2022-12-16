aa9db02492e19ccdf305804c568573dcfefde93a
Jim Hodapp <james.hodapp@gmail.com>
2022-12-16 13:33:49 -0600

    Require Elixir 1.13.0 or above

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Ambi.MixProject do
   def project do
     [
       app: :ambi,
-      version: "0.1.0",
-      elixir: "~> 1.12.2",
+      version: "0.2.0",
+      elixir: "~> 1.13.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
